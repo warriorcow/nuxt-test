d29672a2784e89dd086d9a9d2cbccfa12623aa31
Ilya Borisov <battersman27@gmail.com>
2024-03-13 20:17:18 +0700

    dadad

---
+++ app.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <button @click="getData">fetchData</button>
+    <div>{{ dataPosts }}</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+const dataPosts = ref(null)
+async function getData() {
+  const { data } = await useAsyncData(
+    'mountains',
+    () => $fetch('https://retoolapi.dev/WkceUB/data')
+  )
+
+  dataPosts.value = data;
+}
+
+
+</script>
